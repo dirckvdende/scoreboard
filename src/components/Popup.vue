a5159318678064e6d22e8503f43d945d5b48a709
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-01-22 10:22:07 +0100

    Initialized Vue

---
+++ src/components/Popup.vue
@@ -0,0 +1,32 @@
+<script lang="ts" setup>
+    const visible = defineModel("visible", { default: false })
+</script>
+
+<template>
+    <div :class="$style.container">
+        <div :class="$style.popup">
+            <div :class="$style.head">
+
+            </div>
+            <div :class="$style.body">
+                
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" module>
+    .container {
+
+        .popup {
+
+            .head {
+
+            }
+
+            .body {
+
+            }
+        }
+    }
+</style>
