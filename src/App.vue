859a2bd35c4232cf02036c331274ad1e74c31a35
dirckvdende <128695295+dirckvdende@users.noreply.github.com>
2026-01-23 11:07:39 +0100

    Better resize test

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,23 @@
     import "@/assets/style.scss";
     import AppScoreboard from './components/AppScoreboard.vue';
     import AppButtons from './components/AppButtons.vue';
+    import { useCssVar, useEventListener } from "@vueuse/core";
+
+    const heightVar = useCssVar("--viewport-height", document.body,
+        { initialValue: "100vh" })
+
+    function updateHeight(): void {
+        heightVar.value = `${window.innerHeight}px`
+    }
+
+    useEventListener("resize", updateHeight)
+    updateHeight()
 </script>
 
 <template>
-    <div :class="$style.container">
+    <div :class="$style.container" :style="{
+        minHeight: `var(--viewport-height)`
+    }">
         <div>
             <AppScoreboard />
             <AppButtons />
@@ -20,7 +33,6 @@
         align-items: center;
         justify-content: center;
         width: 100%;
-        min-height: 100dvh;
         box-sizing: border-box;
         padding:
             calc(1.5em + env(safe-area-inset-top))
